2e8456ebdb195ee207c41dadcc3a884a930466ec
Mathéo NOEL <matheo.noel@viacesi.fr>
2019-11-07 15:59:28 +0100

    Navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,21 +1,53 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <span class="navbar-brand">BDE CESI LYON</span>
+<!-- NAVBAR-->
+<div class="row">
+<div class="col-2 pl-2 mt-2" >
+<img src="../public/images/CESILOGO.png" width="90%" alt="Logo CESI">
+</div>
+<nav class="navbar navbar-expand-lg col-10 navbar-dark bg-primary ">
 
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-            <li class="nav-item active">
-                <a class="nav-link" href={{route('activites')}}>Activités<span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href={{route('boutique')}}>Boutique</a>
-            </li>
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="CESI !" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Rechercher</button>
-        </form>
-    </div>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="navbar-brand ">
+            <!-- Logo Text -->
+            <span class="text-uppercase font-weight-bold mr-auto">BDE CESI</span>
+        </div>
+        <div class="collapse navbar-collapse " id="navbarTogglerDemo03">
+                <ul class="navbar-nav navbar-center">
+                    <li class="nav-item ">
+                        <a class="nav-link" href={{route('activites')}}>Activités<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href={{route('boutique')}}>Boutique</a>
+                    </li>
+                </ul>
+        </div>
+            <div class="collapse navbar-collapse " id="navbarTogglerDemo03">
+                <ul class="navbar-nav text-white ml-auto">
+                    <li class="nav-item text-white">
+                        <a class="nav-link">Inscription</a>
+                    </li>
+                    <li class="nav-item text-white">
+                        <a class="nav-link">Connexion</a>
+                    </li>
+                </ul>
+            <!--<form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="CESI !" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Rechercher</button>
+            </form> -->
+        </div>
+
 </nav>
+</div>
+<style>
+    .navbar-center {
+        position: absolute;
+        left: 50%;
+        transform: translatex(-50%);
+    }
+    .row{
+        margin-right: 0;
+        margin-left: 0;
+    }
+</style>
+
