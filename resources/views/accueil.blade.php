e662f3ed8f647c41b6ac77b628f2e8f63a1da2fd
ClappeB <bruno.clappe@viacesi.fr>
2019-11-08 13:16:15 +0100

    Mise en place de l'authentification

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -1,4 +1,9 @@
 @extends('layouts.master')
+@section('title', 'Accueil')
+
+@section('ajoutHead')
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+@endsection
 
 @section('content')
 
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,10 +4,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Accueil : Projet_web beginning !</title>
+    <title>@yield('title')</title>
 
     <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    @yield('ajoutHead')
 
     <!-- Styles -->
     @include('layouts.cesi_graphique')
