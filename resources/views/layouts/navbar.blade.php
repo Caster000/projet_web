efcf2201b11b0b260f7c43976e16d6ac6df0f7b2
Mathéo NOEL <matheo.noel@viacesi.fr>
2019-11-08 11:47:02 +0100

    Navbar&footer fait

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,53 +1,54 @@
 <!-- NAVBAR-->
-<div class="row">
-<div class="col-2 pl-2 mt-2" >
-<img src="../public/images/CESILOGO.png" width="90%" alt="Logo CESI">
-</div>
-<nav class="navbar navbar-expand-lg col-10 navbar-dark bg-primary ">
 
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="navbar-brand ">
-            <!-- Logo Text -->
-            <span class="text-uppercase font-weight-bold mr-auto">BDE CESI</span>
-        </div>
-        <div class="collapse navbar-collapse " id="navbarTogglerDemo03">
-                <ul class="navbar-nav navbar-center">
+
+<div class="row m-1 menu navbarbody">
+    <div class="col-2 pl-4 mt-2">
+        <img src="../public/images/CESILOGO.png" width="70%" alt="Logo CESI">
+    </div>
+    <div class="col-10">
+        <nav class="navbar navbar-expand-lg navbar-custom ">
+            <a class="navbar-brand ">
+                <!-- Logo Text -->
+                <span class="text-uppercase font-weight-bold mr-auto navbarbody">BDE CESI</span>
+            </a>
+
+
+            <button class="navbarcolotoggler navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">Menu
+
+            </button>
+
+            <div class="collapse navbar-collapse font-weight-bold " id="collapsibleNavbar">
+                <ul class="navbar-nav  mx-auto">
                     <li class="nav-item ">
-                        <a class="nav-link" href={{route('activites')}}>Activités<span class="sr-only">(current)</span></a>
+                        <a class="nav-link">Accueil<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link " href={{route('activites')}}>Activités<span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href={{route('boutique')}}>Boutique</a>
                     </li>
                 </ul>
-        </div>
-            <div class="collapse navbar-collapse " id="navbarTogglerDemo03">
-                <ul class="navbar-nav text-white ml-auto">
-                    <li class="nav-item text-white">
+
+
+                <ul class="navbar-nav   ml-auto" >
+                    <li class="nav-item">
                         <a class="nav-link">Inscription</a>
                     </li>
-                    <li class="nav-item text-white">
+                    <li class="nav-item">
                         <a class="nav-link">Connexion</a>
                     </li>
                 </ul>
-            <!--<form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="CESI !" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Rechercher</button>
-            </form> -->
-        </div>
+                <!--<form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="CESI !" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Rechercher</button>
+                </form> -->
+            </div>
 
-</nav>
+
+        </nav>
+    </div>
 </div>
-<style>
-    .navbar-center {
-        position: absolute;
-        left: 50%;
-        transform: translatex(-50%);
-    }
-    .row{
-        margin-right: 0;
-        margin-left: 0;
-    }
-</style>
+
+
 
