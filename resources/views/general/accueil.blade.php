232a0b9796a1d34413dea39e283ff52ce4de473b
Baptiste Chevallier <baptiste.chevallier@viacesi.fr>
2019-11-09 22:04:45 +0100

    modification accueil

--- resources/views/general/accueil.blade.php
+++ resources/views/general/accueil.blade.php
@@ -29,19 +29,19 @@
         <div class="card-body">
             <div class="row justify-content-center">
                 <div class="col-sm-6  col-md-4 col-lg-3 m-2 card" style="width: 18rem;">
-                    <img class="card-img-top" src="..." alt="Card image cap">
+                    <img class="card-img-top" src="/projet_web/public/images/activite/activite1.png" alt="Card image cap">
                     <div class="card-body">
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
                 </div>
                 <div class="col-sm-6  col-md-4 col-lg-3 m-2 card" style="width: 18rem;">
-                    <img class="card-img-top" src="..." alt="Card image cap">
+                    <img class="card-img-top" src="/projet_web/public/images/activite/activite1.png" alt="Card image cap">
                     <div class="card-body">
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
                 </div>
                 <div class="col-sm-6  col-md-4 col-lg-3 m-2 card" style="width: 18rem;">
-                    <img class="card-img-top" src="..." alt="Card image cap">
+                    <img class="card-img-top" src="/projet_web/public/images/activite/activite1.png" alt="Card image cap">
                     <div class="card-body">
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
