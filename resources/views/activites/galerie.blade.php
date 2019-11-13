05a6533ff06860c988cb6a7e8d2a124c28df3c6d
ClappeB <bruno.clappe@viacesi.fr>
2019-11-13 15:43:58 +0100

    Invisibilisation des boutons selon les rôles dans galerie

--- resources/views/activites/galerie.blade.php
+++ resources/views/activites/galerie.blade.php
@@ -8,58 +8,31 @@
     </div>
     <div class="row justify-content-center">
         @foreach($galerie as $photo)
-            <div class="card col-lg-6 col-sm-4  mb-3 mr-4 ml-4 photo2 border border-dark">
-                <a href="{{ URL::action('PhotoController@image',  [$activite->id_activite,$photo->titre]) }}" data-lity><img
-                        src="/projet_web/public/{{$photo->urlImage}}" class="card-img-top " alt="{{$photo->titre}}">
-                </a>
-                @if(auth()->user()->id_role===\App\Role::where('role','BDE')->first()->id_role || auth()->user()->id_role===\App\Role::where('role','CESI')->first()->id_role)
-                    <div class="row">
+            @if($photo->visible==1 || auth()->user()->id_role===\App\Role::where('role','BDE')->first()->id_role || auth()->user()->id_role===\App\Role::where('role','CESI')->first()->id_role)
+                <div class="card col-lg-6 col-sm-4  mb-3 mr-4 ml-4 photo2 border border-dark">
+                    <a href="{{ URL::action('PhotoController@image',  [$activite->id_activite,$photo->titre]) }}" data-lity><img
+                            src="/projet_web/public/{{$photo->urlImage}}" class="card-img-top " alt="{{$photo->titre}}">
+                    </a>
                     @if(auth()->user()->id_role===\App\Role::where('role','BDE')->first()->id_role)
+                    <div class="row">
                         @if(!($photo->visible===1))
                             <a href="{{route('photoRendreVisible', $photo->id_photo)}}" class="btn btn-warning btn-sm col-lg-6 text-bold rounded-0">Invisible</a>
                         @else
                             <a href="{{route('photoRendreInvisible', $photo->id_photo)}}" class="btn btn-warning btn-sm col-lg-6 text-bold rounded-0">Visible</a>
                         @endif
                         <a href="{{route('deletePhoto',$photo->id_photo)}}" class="btn btn-danger btn-sm col-lg-6 rounded-0">Supprimer</a>
-                    @else
+                    </div>
+                    @elseif(auth()->user()->id_role===\App\Role::where('role','CESI')->first()->id_role)
+                    <div class="row">
                         @if(!($photo->visible===1))
                             <a href="{{route('photoRendreVisible', $photo->id_photo)}}" class="btn btn-warning btn-sm col-lg-12 text-bold rounded-0">Invisible</a>
                         @else
                             <a href="{{route('photoRendreInvisible', $photo->id_photo)}}" class="btn btn-warning btn-sm col-lg-12 text-bold rounded-0">Visible</a>
                         @endif
-                    @endif
                     </div>
-                @endif
-{{--                <div id="{{$photo->titre}}" style="background:#fff" class="lity-hide 1">--}}
-{{--                        <div>{{$photo->titre}}jj</div>--}}
-{{--                        <img src="/projet_web/public/{{$photo->urlImage}}" class="card-img-top " alt="{{$photo->titre}}">--}}
-{{--                </div>--}}
-
-{{--                <div id="comment-box-sep">--}}
-{{--                    <a href="/projet_web/public/{{$photo->urlImage}}" data-sub-html='<div>{{$photo->titre}}"</div>'>--}}
-{{--                        <img src="/projet_web/public/{{$photo->urlImage}}" class="card-img-top " alt="{{$photo->titre}}">--}}
-{{--                    </a>--}}
-
-{{--                </div>--}}
-                {{--                    <a href="#myModal<a href="https://www.jqueryscript.net/tags.php?/Modal/">Modal</a>" data-lity>Launch</a>--}}
-
-                {{--                    <div id="myModal" class="lity-hide">--}}
-                {{--                        Modal content here--}}
-                {{--                    </div>--}}
-{{--                <div class="lity" role="dialog" aria-label="Dialog Window (Press escape to close)" tabindex="-1">--}}
-{{--                    <div class="lity-wrap" data-lity-close role="document">--}}
-{{--                        <div class="lity-loader" aria-hidden="true">Loading...</div>--}}
-{{--                        <div class="lity-container">--}}
-{{--                            <div class="lity-content"></div>--}}
-{{--                            <button class="lity-close" type="button" aria-label="Close (Press escape to close)"--}}
-{{--                                    data-lity-close>&times;--}}
-{{--                            </button>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-
-            </div>
-
+                    @endif
+                </div>
+            @endif
         @endforeach
     </div>
 
