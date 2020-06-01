d16f14e4a910b08aad512b48ee1c0fd6cae81579
Baptiste Chevallier <baptiste.chevallier@viacesi.fr>
2020-06-01 21:49:48 +0200

    path-correction

--- resources/views/layouts/cesi_graphique.blade.php
+++ resources/views/layouts/cesi_graphique.blade.php
@@ -1,25 +1,25 @@
 {{-- Icône s'affichant dans l'onglet --}}
-<link rel="icon" href="/public/images/icone.ico">
+<link rel="icon" href="/images/icone.ico">
 
 {{-- Bootstrap et font-awesome --}}
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
 {{-- CSS de la navbar et du footer --}}
-<link rel="stylesheet" type="text/css" href="/public/css/navbar.css"/>
-<link rel="stylesheet" type="text/css" href="/public/css/footer.css"/>
+<link rel="stylesheet" type="text/css" href="/css/navbar.css"/>
+<link rel="stylesheet" type="text/css" href="/css/footer.css"/>
 
 <!--<link rel="stylesheet" type="text/css" href="/projet_web/public/css/boutique.css"/>-->
-<link rel="stylesheet" type="text/css" href="/public/css/dropzone.css"/>
+<link rel="stylesheet" type="text/css" href="/css/dropzone.css"/>
 
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/lity/2.4.0/lity.min.css"/>
 <link href="https://fonts.googleapis.com/css?family=Quicksand&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Oswald&display=swap" rel="stylesheet">
-<link rel="stylesheet" type="text/css" href="/public/css/cookiebar.css"/>
-<link rel="stylesheet" type="text/css" href="/public/css/mentions.css">
-<link rel="stylesheet" type="text/css" href="/public/css/cgv.css">
-<link rel="stylesheet" type="text/css" href="/public/css/accueil.css">
+<link rel="stylesheet" type="text/css" href="/css/cookiebar.css"/>
+<link rel="stylesheet" type="text/css" href="/css/mentions.css">
+<link rel="stylesheet" type="text/css" href="/css/cgv.css">
+<link rel="stylesheet" type="text/css" href="/css/accueil.css">
 
-<!-- A TOUJOURS ADD EN DERNIER--> <link rel="stylesheet" type="text/css" href="/public/css/global.css"/>
+<!-- A TOUJOURS ADD EN DERNIER--> <link rel="stylesheet" type="text/css" href="/css/global.css"/>
 
 
